0f22fd109766e765adea6a300b1863347afdebab
Omid Ayoobe <OAyoobe@spartaglobal.com>
2019-08-15 12:22:34 +0100

    fixed error 2 regarding platform

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,7 +10,7 @@ describe 'node::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', 'xenial64'
+    platform 'ubuntu', '16.4'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
