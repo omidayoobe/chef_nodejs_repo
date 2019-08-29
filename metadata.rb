7c4ec4e166b928ddbb7cc36410b1034f077c51e5
Omid Ayoobe <OAyoobe@spartaglobal.com>
2019-08-29 15:27:23 +0100

    added change4

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures node'
 long_description 'Installs/Configures node'
 version '0.1.0'
-chef_version '>= 14.0'
+
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
